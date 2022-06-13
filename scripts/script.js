9592aeddb99ed60202e18414ee90f7b34df4a131
matthieu-fr-dev <matthieu27@pm.me>
2022-06-13 11:35:45 +0200

    Finalisation du menu
    Insertion d'une div qui permet de fermer le menu a l'aide de la classe CSS.

--- scripts/script.js
+++ scripts/script.js
@@ -4,19 +4,19 @@ const menuBtn = document.getElementById("more");
 const main = document.getElementsByTagName("main")[0];
 // Récuperation de l'overlay du Menu
 const menuOverlay = document.getElementsByTagName("nav")[0];
+// Récuperation du body
+const body = document.getElementsByTagName("body")[0];
+// Récuperation de la div qui sert a fermer le menu
+const menuCloseDiv = document.getElementsByTagName("div")[0];
 
 // Ajout de l'évènement click sur le Menu
 menuBtn.addEventListener("click", activateMenu);
+menuCloseDiv.addEventListener("click", activateMenu);
 
 /**
  * Detecte si le menu est ouvert ou fermé et gère l'affichage
  */
 function activateMenu() {
-  let menuOpened = menuBtn.classList.contains("active");
-
-  let menuCloser = document.createElement("div");
-
-  if (!menuOpened) {
-    menuOverlay.classList.toggle("active");
-  }
+  menuOverlay.classList.toggle("active");
+  menuCloseDiv.classList.toggle("active");
 }
